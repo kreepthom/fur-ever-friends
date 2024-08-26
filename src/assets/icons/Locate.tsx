f4ac38404e724e49c1b5845d20a30015a584af7e
Eday Daza <ejdaza42@gmail.com>
2024-08-25 21:24:43 -0500

    Feat: Add icon components

---
+++ src/assets/icons/Locate.tsx
@@ -0,0 +1,27 @@
+import { SVGProps } from "react";
+
+export const Locate = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={800}
+    height={800}
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 21c3.5-3.6 7-6.824 7-10.8C19 6.224 15.866 3 12 3s-7 3.224-7 7.2 3.5 7.2 7 10.8Z"
+    />
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+    />
+  </svg>
+);
